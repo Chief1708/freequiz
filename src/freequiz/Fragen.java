130b5b7f36c3de8ede383d11fff18af580e8ef8b
Darius Musiolik <Musiolik@Secure-Solutions.org>
2019-05-17 16:16:39 +0200

    Added 'Fragen' Class

---
+++ src/freequiz/Fragen.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package freequiz;
+
+/**
+ *
+ * @author dmusiolik
+ */
+public class Fragen {
+    
+    public static String[] fragen = new String[5];
+    
+    public Fragen() {
+        fragen[0] = "";
+        fragen[1] = "";
+        fragen[2] = "";
+        fragen[3] = "";
+        fragen[4] = "";
+    }
+    
+}
