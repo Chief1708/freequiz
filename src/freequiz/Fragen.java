9db163ed7271c3d9cc0f740d9d17fafb61fdb11a
Darius Musiolik <Musiolik@Secure-Solutions.org>
2019-05-17 16:25:47 +0200

    Added Frage Array

--- src/freequiz/Fragen.java
+++ src/freequiz/Fragen.java
@@ -11,14 +11,74 @@ package freequiz;
  */
 public class Fragen {
     
-    public static String[] fragen = new String[5];
+    public static String[][][][][][] fragen = new String[5][1][1][1][1][1];
     
     public Fragen() {
-        fragen[0] = "";
-        fragen[1] = "";
-        fragen[2] = "";
-        fragen[3] = "";
-        fragen[4] = "";
+        //Frage 1
+        fragen[0][0][0][0][0][0] = "";
+        //Antwort 1
+        fragen[0][1][0][0][0][0] = "";
+        //Antwort 2
+        fragen[0][0][1][0][0][0] = "";
+        //Antwort 3
+        fragen[0][0][0][1][0][0] = "";
+        //Antwort 4
+        fragen[0][0][0][0][1][0] = "";
+        //Richtige Antwort
+        fragen[0][0][0][0][0][1] = "A";
+        
+        //Frage 2
+        fragen[1][0][0][0][0][0] = "";
+        //Antwort 1
+        fragen[1][1][0][0][0][0] = "";
+        //Antwort 2
+        fragen[1][0][1][0][0][0] = "";
+        //Antwort 3
+        fragen[1][0][0][1][0][0] = "";
+        //Antwort 4
+        fragen[1][0][0][0][1][0] = "";
+        //Richtige Antwort
+        fragen[0][0][0][0][0][1] = "A";
+        
+        
+        //Frage 3
+        fragen[2][0][0][0][0][0] = "";
+        //Antwort 1
+        fragen[2][1][0][0][0][0] = "";
+        //Antwort 2
+        fragen[2][0][1][0][0][0] = "";
+        //Antwort 3
+        fragen[2][0][0][1][0][0] = "";
+        //Antwort 4
+        fragen[2][0][0][0][1][0] = "";
+        //Richtige Antwort
+        fragen[0][0][0][0][0][1] = "A";
+        
+        //Frage 4
+        fragen[3][0][0][0][0][0] = "";
+        //Antwort 1
+        fragen[3][1][0][0][0][0] = "";
+        //Antwort 2
+        fragen[3][0][1][0][0][0] = "";
+        //Antwort 3
+        fragen[3][0][0][1][0][0] = "";
+        //Antwort 4
+        fragen[3][0][0][0][1][0] = "";
+        //Richtige Antwort
+        fragen[0][0][0][0][0][1] = "A";
+        
+        //Frage 5
+        fragen[4][0][0][0][0][0] = "";
+        //Antwort 1
+        fragen[4][1][0][0][0][0] = "";
+        //Antwort 2
+        fragen[4][0][1][0][0][0] = "";
+        //Antwort 3
+        fragen[4][0][0][1][0][0] = "";
+        //Antwort 4
+        fragen[4][0][0][0][1][0] = "";
+        //Richtige Antwort
+        fragen[0][0][0][0][0][1] = "A";
     }
     
 }
