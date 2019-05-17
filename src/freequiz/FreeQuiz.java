7c5c45bc10f68c31b12053dcd10f7e3d29db53b9
Darius Musiolik <Musiolik@Secure-Solutions.org>
2019-05-17 14:43:31 +0200

    Initial Commit

---
+++ src/freequiz/FreeQuiz.java
@@ -0,0 +1,16 @@
+/*
+ * GPLv3; Free Software
+ */
+package freequiz;
+
+/**
+ *
+ * @author dmusiolik, cfchong & jwolter
+ */
+public class FreeQuiz {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
